a8a01acd08a252eecc8ae1789eab6ee6b58e4b41
Kenneth Garcia <kennethgarcia12@hotmail.com>
2019-03-11 11:38:02 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="php Contact Form">
+    <meta name="keywords" content="php, server name, client info">
+    <meta name="author" content="Kenneth Garcia">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!--BOOTSWATCH link-->
+    <link rel="stylesheet" href="./resources/bootstrap.min.css">
+    <title>Contact Us</title>
+  </head><!--end of head-->
+  <body>
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <div class="navbar-header">
+          <a href="index.php" class="navbar-brand">MY Website</a>
+        </div>
+      </div><!--end of container-->
+    </nav><!--end of nav-->
+    <form class="" action="index.html" method="post">
+      <div class="form-group">
+        <label>Name</label>
+        <input type="text" name="name" class="form-control">
+      </div><!--end of form-group-->
+
+      <div class="form-group">
+        <label>Email</label>
+        <input type="text" name="email" class="form-control">
+      </div><!--end of form-group-->
+
+      <div class="form-group">
+        <label>Name</label>
+        <input type="text" name="name" class="form-control">
+      </div><!--end of form-group-->
+    </form><!--end of form-->
+  </body>
+</html>
